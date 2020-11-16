5a5abb9a4b7409e35774ac3773c05f288426206e
Novixous <anonymousvhb@gmail.com>
2020-11-17 03:13:27 +0700

    add test

---
+++ test_client.py
@@ -0,0 +1,7 @@
+import socket
+server_stream_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_stream_socket.connect(("127.0.0.1", 12345))
+server_stream_socket.send(b"start-D:/Downloads/detection/detection/video/")
+msg = server_stream_socket.recv(1024)
+print(msg)
+server_stream_socket.close()
